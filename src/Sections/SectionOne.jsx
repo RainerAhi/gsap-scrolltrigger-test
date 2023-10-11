73a9c50400ebfb069fe74f7d74d518a47b4c5294
StudenxFinance <studenxfinance@gmail.com>
2023-10-11 19:24:27 +0300

    fix 4

--- src/Sections/SectionOne.jsx
+++ src/Sections/SectionOne.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export const SectionOne = () => {
     return (
         <section className="first-section" >
-            SectionOne
-            <button className="button--test" id="button--test" >TAP ME</button>
+                <button className="button--test" id="button--test" >TAP ME</button>
         </section>
     )
 }
